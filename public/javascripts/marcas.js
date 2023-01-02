b95d92a41abbab4862041011cfaa566f99ce8651
gaslaf <lafuentegaston.gl@gmail.com>
2023-01-02 13:14:34 -0300

    agregando otro banner

---
+++ public/javascripts/marcas.js
@@ -0,0 +1,10 @@
+new Glider(document.querySelector('.logos'), {
+    slidesToShow:9,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: '.puntos',
+    arrows: {
+      prev: '.glider-ant',
+      next: '.glider-sig'
+    }
+  });
